7bcd4a618e5bd4ec02e1f736970c41063d2ffbd1
sumonDev13 <sumonmondal1530765@gmail.com>
2024-03-29 15:28:14 +0600

    wip banner

--- frontend/src/Components/slider/SliderImages.jsx
+++ frontend/src/Components/slider/SliderImages.jsx
@@ -1,5 +1,5 @@
 export const images = [
-    "https://i.postimg.cc/v8nyCQVX/banner.png",
-    "https://i.postimg.cc/v8nyCQVX/banner.png",
-    "https://i.postimg.cc/v8nyCQVX/banner.png",
+    "https://i.postimg.cc/wxcd8xqg/banner1.jpg",
+    "https://i.postimg.cc/XJRZb396/food.png",
+    "https://i.postimg.cc/7Z55tmC2/times.png",
   ];
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useReducer } from 'react';
-import axios from 'axios';
-import Product from '../../Components/Product/Product';
-import { Helmet } from 'react-helmet-async';
-import Loading from '../../Components/Loading/Loading';
-import ErrorPage from '../../Components/ErrorPage/ErrorPage';
-import Categories from '../../Components/Categories/Categories';
-import Slider from '../../Components/slider/Slider';
-import { images } from '../../Components/slider/SliderImages';
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+import Product from "../../Components/Product/Product";
+import Loading from "../../Components/Loading/Loading";
+import ErrorPage from "../../Components/ErrorPage/ErrorPage";
+import Categories from "../../Components/Categories/Categories";
+import Slider from "../../Components/slider/Slider";
+import { images } from "../../Components/slider/SliderImages";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'FETCH_REQUEST':
+    case "FETCH_REQUEST":
       return { ...state, loading: true };
-    case 'FETCH_SUCCESS':
+    case "FETCH_SUCCESS":
       return { ...state, products: action.payload, loading: false };
-    case 'FETCH_FAIL':
+    case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
@@ -25,17 +24,17 @@ function Home() {
   const [{ loading, error, products }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
-    error: '',
+    error: "",
   });
   // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' });
+      dispatch({ type: "FETCH_REQUEST" });
       try {
-        const result = await axios.get('/api/products');
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
       }
       // setProducts(result.data);
     };
@@ -44,11 +43,10 @@ function Home() {
 
   return (
     <div className="home__component">
-      <div className="text-center pb-4 pt-8">
-        <Helmet>
-          <title>EcomBidding</title>
-        </Helmet>
+      <div className="py-8">
         <Slider images={images} />
+      </div>
+      <div className="text-center pb-4 pt-8">
         <h2 className="text-gray-600 flex justify-center align-middle text-center md:inline-block border-b-2 w-4/12 border-solid border-lightgray text-4xl px-2 mx-auto">
           Listed Products
         </h2>
